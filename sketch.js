ba56a145d86d9eedbe052113a4ec4bd8c69de897
killerNEYMAR <70897820+killerNEYMAR@users.noreply.github.com>
2020-10-22 14:46:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var dog;
+var happyDog;
+var database;
+var foodS;
+var foodStock;
+
+
+function preload()
+{
+  dog = loadImage("dogImg.png");
+  happyDog = loadImage("dogImg1.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
